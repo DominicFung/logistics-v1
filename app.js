ad6024e70ff3e43cd27309f0a694b26f1ceab923
Dom Fung <fung_dominic@hotmail.com>
2021-02-16 15:42:05 -0500

    ex 6 checks out

--- app.js
+++ app.js
@@ -20,20 +20,41 @@ const legend5 = require("./inputs/ex5/legend.json")
 const request5 = require("./inputs/ex5/request.json")
 const maxCapacity5 = require("./inputs/ex5/maxCap.json")
 
+const legend6 = require("./inputs/ex6/legend.json")
+const request6 = require("./inputs/ex6/request.json")
+const maxCapacity6 = require("./inputs/ex6/maxCap.json")
 
 console.log("-- START --")
 
+/**
+ * Note: Backtracking example
+ */
 //console.log("=== EXAMPLE 1 ===")
 //console.log( logistic(legend1, request1, maxCapacity1) )
 
 //console.log("=== EXAMPLE 2 ===")
 //console.log( logistic(legend2, request2, maxCapacity2) )
 
+/**
+ * Note: York Email 1; Sample-play.xlsx
+ */
 //console.log("=== EXAMPLE 3 ===")
 //console.log( logistic(legend3, request3, maxCapacity3) )
 
+/**
+ * Note: York Email 2; Sample-play - Copy.xlsx
+ */
 //console.log("=== EXAMPLE 4 ===")
 //console.log( logistic(legend4, request4, maxCapacity4) )
 
-console.log("=== EXAMPLE 5 ===")
-console.log( logistic(legend5, request5, maxCapacity5) )
+/**
+ * Note: York Email 2; Sample-play - Copy(2).xlsx
+ */
+//console.log("=== EXAMPLE 5 ===")
+//console.log( logistic(legend5, request5, maxCapacity5) )
+
+/**
+ * Note: York Email 2; Sample-play - Copy(3).xlsx
+ */
+console.log("=== EXAMPLE 6 ===")
+console.log( logistic(legend6, request6, maxCapacity6) )
